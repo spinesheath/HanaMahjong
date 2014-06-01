81f65e772c8e8bf4b5c3a8358a0e933a91190ddd
spinesheath <spines@web.de>
2014-06-01 17:35:11 +0200

    Setup for Analysis module
    Main project, project for testing the public interface and project for
    internal tests.

---
+++ Spines.Mahjong/Spines.Mahjong.Analysis.InternalTests/MeldArrangementTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spines.Mahjong.Analysis;
+
+namespace Spines.Mahjong.Analysis.InternalTests
+{
+  [TestClass]
+  public class MeldArrangementTests
+  {
+  }
+}
---
+++ Spines.Mahjong/Spines.Mahjong.Analysis.Tests/ShantenCalculatorTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Spines.Mahjong.Analysis.Tests
+{
+  [TestClass]
+  public class ShantenCalculatorTests
+  {
+  }
+}
