e461f4a304374d640a8e3654f8fb7c4ac04afb01
spinesheath <spines@web.de>
2014-06-01 20:49:17 +0200

    PublicKey for InternalTests

--- Spines.Mahjong/Spines.Mahjong.Analysis/Properties/AssemblyInfo.cs
+++ Spines.Mahjong/Spines.Mahjong.Analysis/Properties/AssemblyInfo.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using System.Security.Cryptography.X509Certificates;
+
 [assembly: AssemblyTitle("Spines.Mahjong.Analysis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -35,4 +37,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Spines.Mahjong.Analysis.InternalTests")]
+[assembly: InternalsVisibleTo("Spines.Mahjong.Analysis.InternalTests,PublicKey="+
+"00240000048000009400000006020000002400005253413100040000010001002f388592d01391"+
+"6f47be930a324ac604fc0142f79be1435100e97c3591a004138cb394b1820a24b60b2bb0817fa7"+
+"1387d761bcc7344396572905b49af4c4b84e63aab7f3c03aa7fd7472bb23c7aa747ee192ccbc65"+
+"8202231ca66e36c452bcf64f7639c55171cb218d8c32e86a26701ea19fd449f7dd35a0bb5ff589"+
+"a145d296")]
+
+
