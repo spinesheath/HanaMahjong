c0bed80ceba60e725f0a56721496c257fb41767c
spinesheath <spines@web.de>
2015-01-25 20:43:27 +0100

    Refactoring

--- Spines.Tenhou/Spines.Tenhou.Client/WallGenerator.cs
+++ Spines.Tenhou/Spines.Tenhou.Client/WallGenerator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using Spines.Utility;
@@ -11,41 +12,43 @@ namespace Spines.Tenhou.Client
   public class WallGenerator
   {
     // Random generator
-    private Mt19937Ar R = new Mt19937Ar();
+    private readonly Mt19937Ar _r = new Mt19937Ar();
 
-    private uint[] SeedValues = new uint[624];
-    private int[] Wall = new int[136];
-    private int[] Dice = new int[2];
+    private readonly uint[] _seedValues = new uint[624];
+    private readonly int[] _wall = new int[136];
+    private readonly int[] _dice = new int[2];
 
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="Seed"></param>
-    public WallGenerator(string Seed)
+    /// <param name="seed"></param>
+    public WallGenerator(string seed)
     {
       unchecked
       {
-        int DelimiterPos = Seed.IndexOf(",");
+        var delimiterPos = seed.IndexOf(",", StringComparison.Ordinal);
 
-        string SeedText = Seed.Substring(DelimiterPos + 1);
+        var seedText = seed.Substring(delimiterPos + 1);
 
         // mt19937ar-sha512-n288-base64,*data*
-        byte[] SeedBytes;
 
-        if (Seed.IndexOf("mt19937ar-sha512-n288-base64") == 0)
+        if (seed.IndexOf("mt19937ar-sha512-n288-base64", StringComparison.Ordinal) == 0)
         {
-          SeedBytes = Convert.FromBase64String(SeedText);
+          byte[] seedBytes = Convert.FromBase64String(seedText);
 
           // Convert to UInt32 array
-          for (int i = 0; i < SeedValues.Length; i++) SeedValues[i] = (uint)(SeedBytes[i * 4 + 0] << 0) |
-                                                                      (uint)(SeedBytes[i * 4 + 1] << 8) |
-                                                                      (uint)(SeedBytes[i * 4 + 2] << 16) |
-                                                                      (uint)(SeedBytes[i * 4 + 3] << 24);
+          for (var i = 0; i < _seedValues.Length; i++)
+          {
+            _seedValues[i] = (uint)(seedBytes[i * 4 + 0] << 0) |
+                             (uint)(seedBytes[i * 4 + 1] << 8) |
+                             (uint)(seedBytes[i * 4 + 2] << 16) |
+                             (uint)(seedBytes[i * 4 + 3] << 24);
+          }
         }
         else
         {
           // Old style seed value
-          SeedValues = DecompositeHexList(SeedText);
+          _seedValues = DecompositeHexList(seedText);
         }
       }
     }
@@ -53,55 +56,55 @@ namespace Spines.Tenhou.Client
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="Text"></param>
+    /// <param name="text"></param>
     /// <returns></returns>
-    private uint[] DecompositeHexList(string Text)
+    private static uint[] DecompositeHexList(string text)
     {
       unchecked
       {
-        string[] delimiter = new string[] { "," };
-        string[] Temp;
-        uint[] Result = null;
+        string[] delimiter = { "," };
 
-        if (Text == null) return null;
+        if (text == null) return null;
 
-        Temp = Text.Split(delimiter, StringSplitOptions.None);
-        Result = new uint[Temp.Length];
+        var temp = text.Split(delimiter, StringSplitOptions.None);
+        var result = new uint[temp.Length];
 
-        for (int i = 0; i < Temp.Length; i++)
+        for (var i = 0; i < temp.Length; i++)
         {
-          int Index = Temp[i].IndexOf('.');
-          if (Index >= 0) Temp[i] = Temp[i].Substring(0, Index);
+          var index = temp[i].IndexOf('.');
+          if (index >= 0) temp[i] = temp[i].Substring(0, index);
 
-          Result[i] = Convert.ToUInt32(Temp[i], 16);
+          result[i] = Convert.ToUInt32(temp[i], 16);
         }
 
-        return Result;
+        return result;
       }
     }
 
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="Index"></param>
-    public void Generate(int Index)
+    /// <param name="gameIndex">The index of the game within the match.</param>
+    public void Generate(int gameIndex)
     {
       unchecked
       {
-// Init random generator by seed array
-        R.InitByArray(SeedValues);
+        _r.InitByArray(_seedValues.Select(x => (int)x).ToArray());
 
-        uint[] rnd = new uint[64 / 4 * 9]; // 144
+        var rnd = new uint[64 / 4 * 9]; // 144
 
         // SHA512 hash
-        for (int j = 0; j < (Index + 1); j++)
+        for (var j = 0; j < gameIndex + 1; j++)
         {
-          uint[] src = new uint[64 / 4 * 9 * 2];
+          var src = new uint[64 / 4 * 9 * 2];
 
-          for (int i = 0; i < src.Length; i++) src[i] = R.GetNext();
+          for (var i = 0; i < src.Length; i++)
+          {
+            src[i] = (uint) _r.GetNext();
+          }
 
-          byte[] srcbyte = new byte[src.Length * 4];
-          for (int i = 0; i < src.Length; i++)
+          var srcbyte = new byte[src.Length * 4];
+          for (var i = 0; i < src.Length; i++)
           {
             srcbyte[i * 4 + 0] = Convert.ToByte((src[i] >> 0) & 0xFF);
             srcbyte[i * 4 + 1] = Convert.ToByte((src[i] >> 8) & 0xFF);
@@ -109,43 +112,47 @@ namespace Spines.Tenhou.Client
             srcbyte[i * 4 + 3] = Convert.ToByte((src[i] >> 24) & 0xFF);
           }
 
-          for (int i = 0; i < 9; ++i)
+          for (var i = 0; i < 9; ++i)
           {
-            SHA512Managed Context = new SHA512Managed();
-
-            byte[] datatohash = new byte[128];
-            for (int k = 0; k < 128; k++) datatohash[k] = srcbyte[k + i * 128];
-
-            byte[] hash = Context.ComputeHash(datatohash);
-
-            for (int k = 0; k < 16; k++)
+            using(var context = new SHA512Managed())
             {
-              rnd[i * 16 + k] = (uint)(hash[k * 4 + 0] << 0) |
-                                (uint)(hash[k * 4 + 1] << 8) |
-                                (uint)(hash[k * 4 + 2] << 16) |
-                                (uint)(hash[k * 4 + 3] << 24);
+              var datatohash = new byte[128];
+              for (var k = 0; k < 128; k++)
+              {
+                datatohash[k] = srcbyte[k + i * 128];
+              }
+
+              var hash = context.ComputeHash(datatohash);
+
+              for (var k = 0; k < 16; k++)
+              {
+                rnd[i * 16 + k] = (uint)(hash[k * 4 + 0] << 0) |
+                                  (uint)(hash[k * 4 + 1] << 8) |
+                                  (uint)(hash[k * 4 + 2] << 16) |
+                                  (uint)(hash[k * 4 + 3] << 24);
+              }
             }
           }
         }
 
         // Generate wall
-        for (int i = 0; i < Wall.Length; i++) Wall[i] = i;
+        for (var i = 0; i < _wall.Length; i++) _wall[i] = i;
 
         // Shuffle!
-        for (int i = 0; i < Wall.Length - 1; i++)
+        for (var i = 0; i < _wall.Length - 1; i++)
         {
-          int Src = i;
-          int Dst = i + Convert.ToInt16(rnd[i] % (136 - i));
+          var src = i;
+          var dst = i + Convert.ToInt16(rnd[i] % (136 - i));
 
           // Swap 2 tiles
-          int Swp = Wall[Src];
-          Wall[Src] = Wall[Dst];
-          Wall[Dst] = Swp;
+          var swp = _wall[src];
+          _wall[src] = _wall[dst];
+          _wall[dst] = swp;
         }
 
         // Dice!
-        Dice[0] = 1 + Convert.ToInt16(rnd[135] % 6);
-        Dice[1] = 1 + Convert.ToInt16(rnd[136] % 6);
+        _dice[0] = 1 + Convert.ToInt16(rnd[135] % 6);
+        _dice[1] = 1 + Convert.ToInt16(rnd[136] % 6);
       }
     }
 
@@ -153,12 +160,12 @@ namespace Spines.Tenhou.Client
     /// 
     /// </summary>
     /// <returns></returns>
-    public int[] GetWall()
+    public IEnumerable<int> GetWall()
     {
-      int[] Temp = new int[136];
-      for (int i = 0; i < Wall.Length; i++) Temp[i] = Wall[i];
+      var temp = new int[136];
+      for (var i = 0; i < _wall.Length; i++) temp[i] = _wall[i];
 
-      return Temp;
+      return temp;
     }
 
     /// <summary>
@@ -167,10 +174,10 @@ namespace Spines.Tenhou.Client
     /// <returns></returns>
     public int[] GetDice()
     {
-      int[] Temp = new int[2];
-      for (int i = 0; i < Dice.Length; i++) Temp[i] = Dice[i];
+      var temp = new int[2];
+      for (var i = 0; i < _dice.Length; i++) temp[i] = _dice[i];
 
-      return Temp;
+      return temp;
     }
   }
 
--- Spines.Utility/Mt19937Ar.cs
+++ Spines.Utility/Mt19937Ar.cs
@@ -22,134 +22,135 @@ using System.Linq;
 namespace Spines.Utility
 {
   /// <summary>
-  /// 
   /// </summary>
   public class Mt19937Ar
   {
-    private const int N = 624;
-        private const int M = 397;
+    private const int StateSize = 624;
+    private const int M = 397;
 
-        private const uint MATRIX_A = 0x9908b0df;   // constant vector a
-        private const uint UPPER_MASK = 0x80000000; // most significant w-r bits
-        private const uint LOWER_MASK = 0x7fffffff; // least significant r bits
+    private const uint MatrixA = 0x9908b0df; // constant vector a
+    private const uint UpperMask = 0x80000000; // most significant w-r bits
+    private const uint LowerMask = 0x7fffffff; // least significant r bits
+    private readonly uint[] _mag01 = {0x0, MatrixA};
 
-        private uint[] mt = new uint[N]; // the array for the state vector
-        private uint mti = N + 1;        // means mt[N] is not initialized
-
-        private uint[] mag01 = new uint[2] { 0x0, MATRIX_A };
+    private readonly uint[] _state = new uint[StateSize]; // the array for the state vector
+    private uint _nextStateIndex;
 
     /// <summary>
-    /// 
     /// </summary>
-    /// <param name="Seed"></param>
-        public void Init(uint Seed)
-        {
-          unchecked
-          {
-            mt[0] = Seed & 0xffffffff;
-            for (mti = 1; mti < N; mti++)
-            {
-              mt[mti] = (1812433253 * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
-              // See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier.
-              // In the previous versions, MSBs of the seed affect
-              // only MSBs of the array mt[].
-              // 2002/01/09 modified by Makoto Matsumoto
+    /// <returns></returns>
+    public int GetNext()
+    {
+      if (_nextStateIndex == 0)
+      {
+        CreateNextState();
+      }
 
-              mt[mti] &= 0xffffffff;
-              // for >32 bit machines
-            }
-          }
+      var t = _state[_nextStateIndex];
+      _nextStateIndex = (_nextStateIndex + 1) % StateSize;
+      return unchecked ((int)Temper(t));
+    }
+
+    private void CreateNextState()
+    {
+      for (var k = 0; k < StateSize - M; ++k)
+      {
+        ApplyMatrix(k, k + M, GetUpperLower(k, k + 1));
+      }
+      for (var k = StateSize - M; k < StateSize - 1; ++k)
+      {
+        ApplyMatrix(k, k + (M - StateSize), GetUpperLower(k, k + 1));
+      }
+      ApplyMatrix(StateSize - 1, M - 1, GetUpperLower(StateSize - 1, 0));
+    }
+
+    private void ApplyMatrix(int targetIndex, int sourceIndex, uint matrixIndex)
+    {
+      _state[targetIndex] = _state[sourceIndex] ^ (matrixIndex >> 1) ^ _mag01[matrixIndex & 0x1];
+    }
+
+    private uint GetUpperLower(int upperIndex, int lowerIndex)
+    {
+      return (_state[upperIndex] & UpperMask) | (_state[lowerIndex] & LowerMask);
+    }
+
+    private static uint Temper(uint y)
+    {
+      y ^= (y >> 11);
+      y ^= (y << 7) & 0x9d2c5680;
+      y ^= (y << 15) & 0xefc60000;
+      y ^= (y >> 18);
+      return y;
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="seed"></param>
+    private void Init(uint seed)
+    {
+      _state[0] = seed;
+      for (uint i = 1; i < StateSize; i++)
+      {
+        unchecked
+        {
+          _state[i] = PreviousXor(i) * 1812433253;
+          _state[i] += i;
         }
+      }
+    }
+
+    private uint PreviousXor(uint i)
+    {
+      return _state[i - 1] ^ (_state[i - 1] >> 30);
+    }
 
     /// <summary>
-    /// 
     /// </summary>
-    /// <param name="Seed"></param>
-        public void InitByArray(uint[] Seed)
-        {
-          unchecked
-          {
-            uint i, j, k;
-            Init(19650218);
+    /// <param name="seed"></param>
+    public void InitByArray(IEnumerable<int> seed)
+    {
+      unchecked
+      {
+        var seedArray = seed.Select(x => (uint)x).ToArray();
+        Init(19650218);
 
+        uint i = 1;
+        uint j = 0;
+
+        for (var k = Math.Max(StateSize, seedArray.Length); k > 0; --k)
+        {
+          _state[i] ^= PreviousXor(i) * 1664525;
+          _state[i] += seedArray[j] + j;
+
+          i += 1;
+          j += 1;
+
+          if (i >= StateSize)
+          {
+            _state[0] = _state[StateSize - 1];
             i = 1;
-            j = 0;
-
-            k = (N > (uint)Seed.Length ? N : (uint)Seed.Length);
-            for (; k > 0; k--)
-            {
-              mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1664525)) + Seed[j] + j; // non linear
-              mt[i] &= 0xffffffff; // for WORDSIZE > 32 machines
-
-              i++;
-              j++;
-
-              if (i >= N) { mt[0] = mt[N - 1]; i = 1; }
-              if (j >= Seed.Length) j = 0;
-            }
-
-            for (k = N - 1; k > 0; k--)
-            {
-              mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1566083941)) - i; // non linear
-              mt[i] &= 0xffffffff; // for WORDSIZE > 32 machines
-
-              i++;
-              if (i >= N) { mt[0] = mt[N - 1]; i = 1; }
-            }
-
-            mt[0] = 0x80000000; // MSB is 1; assuring non-zero initial array
           }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public uint GetNext()
-        {
-          unchecked
+          if (j >= seedArray.Length)
           {
-            uint y;
-           
-            // mag01[x] = x * MATRIX_A  for x = 0,1
-
-            if (mti >= N)
-            { // generate N words at one time
-              int kk;
-
-              // if Init() has not been called,
-              // a default initial seed is used
-              if (mti == N + 1) Init(5489);
-
-              for (kk = 0; kk < N - M; kk++)
-              {
-                y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
-                mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[y & 0x1];
-              }
-
-              for (;kk < N - 1; kk++)
-              {
-                y = (mt[kk] & UPPER_MASK)|(mt[kk + 1] & LOWER_MASK);
-                mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1];
-              }
-
-              y = (mt[N-1] & UPPER_MASK)|(mt[0] & LOWER_MASK);
-              mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1];
-
-              mti = 0;
-            }
-         
-            y = mt[mti++];
-
-            // Tempering
-            y ^= (y >> 11);
-            y ^= (y << 7) & 0x9d2c5680;
-            y ^= (y << 15) & 0xefc60000;
-            y ^= (y >> 18);
-
-            return y;
+            j = 0;
           }
         }
 
+        for (var k = StateSize - 1; k > 0; --k)
+        {
+          _state[i] ^= PreviousXor(i) * 1566083941;
+          _state[i] -= i;
+
+          i += 1;
+          if (i < StateSize)
+          {
+            continue;
+          }
+          _state[0] = _state[StateSize - 1];
+          i = 1;
+        }
+        _state[0] = 0x80000000;
+      }
+    }
   }
 }
