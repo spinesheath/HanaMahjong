3299c61a2d50d75db159bd85d6b40c84f72076a0
spinesheath <spines@web.de>
2017-03-10 23:51:19 +0100

    Progressive hand development test

--- Spines.Mahjong/Spines.Mahjong.Analysis.InternalTests/ShantenCalculatorTests.cs
+++ Spines.Mahjong/Spines.Mahjong.Analysis.InternalTests/ShantenCalculatorTests.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using NUnit.Framework;
 using Spines.Mahjong.Analysis.Classification;
 using Spines.Mahjong.Analysis.Combinations;
@@ -65,6 +66,176 @@ namespace Spines.Mahjong.Analysis.InternalTests
           classifier.ClassifyHonors(emptyHonor));
         Assert.That(shanten, Is.GreaterThan(-2));
       }
+    }
+
+    [Test]
+    public void ProgressiveCalculationShouldWork()
+    {
+      var rand = new Random(1);
+      var classifier = new Classifier();
+
+      var concealed = new int[34];
+      var melded = new int[34];
+      var discarded = new int[34];
+      const int meldCount = 0;
+      var shanten = 10;
+      var emptyMelds = new int[0];
+      var actualDraws = 0;
+      for (var i = 0; i < 10000; ++i)
+      {
+        if (shanten == -1 || Enumerable.Range(0, 34).All(a => concealed[a] + melded[a] + discarded[a] == 4))
+        {
+          concealed.Populate(0);
+          melded.Populate(0);
+          discarded.Populate(0);
+          shanten = 10;
+        }
+
+        var t = rand.Next(34);
+        var draw = ToDiscard(t);
+        if (concealed[t] + melded[t] + discarded[t] == 4)
+        {
+          continue;
+        }
+        concealed[t] += 1;
+        actualDraws += 1;
+        if (concealed.Sum() + meldCount * 3 < 14)
+        {
+          continue;
+        }
+
+        var curHand = ToHand(concealed);
+
+        shanten = classifier.ClassifyArrangements(
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(0, 9)),
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(9, 9)),
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(18, 9)),
+            classifier.ClassifyHonors(Enumerable.Repeat(0, 8).Concat(concealed.Slice(27, 7).OrderByDescending(x => x)).ToList()));
+
+        if (shanten == -1)
+        {
+          continue;
+        }
+
+        var ukeIreCount = -1;
+        var bestDiscard = -1;
+        var bestShanten = 10;
+        var bestUkeIre = new int[34];
+        for (var j = 0; j < 34; ++j)
+        {
+          if (concealed[j] == 0)
+          {
+            continue;
+          }
+          concealed[j] -= 1;
+          discarded[j] += 1;
+
+          var handAfterDiscard = ToHand(concealed);
+
+          var s1 = classifier.ClassifyArrangements(
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(0, 9)),
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(9, 9)),
+            classifier.ClassifySuits(emptyMelds, concealed.Slice(18, 9)),
+            classifier.ClassifyHonors(Enumerable.Repeat(0, 8).Concat(concealed.Slice(27, 7).OrderByDescending(x => x)).ToList()));
+
+          if (bestShanten < 10)
+          {
+            Assert.That(s1, Is.AtLeast(shanten));
+            Assert.That(s1, Is.AtMost(shanten + 1));
+          }
+
+          if (s1 > bestShanten)
+          {
+            concealed[j] += 1;
+            discarded[j] -= 1;
+            continue;
+          }
+
+          var ukeIre = new int[34];
+          var u = 0;
+          for (var k = 0; k < 34; ++k)
+          {
+            var used = concealed[k] + melded[k] + discarded[k];
+            if (used == 4)
+            {
+              continue;
+            }
+            concealed[k] += 1;
+
+            var handAfterDraw = ToHand(concealed);
+
+            var s2 = classifier.ClassifyArrangements(
+              classifier.ClassifySuits(emptyMelds, concealed.Slice(0, 9)),
+              classifier.ClassifySuits(emptyMelds, concealed.Slice(9, 9)),
+              classifier.ClassifySuits(emptyMelds, concealed.Slice(18, 9)),
+              classifier.ClassifyHonors(Enumerable.Repeat(0, 8).Concat(concealed.Slice(27, 7).OrderByDescending(x => x)).ToList()));
+
+            if (bestShanten < 10)
+            {
+              Assert.That(s2, Is.AtLeast(s1 - 1));
+              Assert.That(s2, Is.AtMost(s1));
+            }
+
+            concealed[k] -= 1;
+
+            if (s2 < shanten)
+            {
+              ukeIre[k] = 1;
+              u += 4 - used;
+            }
+          }
+
+          if (u > ukeIreCount || s1 < bestShanten)
+          {
+            Assert.That(s1, Is.AtMost(bestShanten));
+
+            bestUkeIre = ukeIre;
+            ukeIreCount = u;
+            bestDiscard = j;
+            bestShanten = s1;
+          }
+
+          concealed[j] += 1;
+          discarded[j] -= 1;
+        }
+
+        var discard = ToDiscard(bestDiscard);
+        var ukeIreTiles = ToHand(bestUkeIre);
+
+        concealed[bestDiscard] -= 1;
+        discarded[bestDiscard] += 1;
+        shanten = bestShanten;
+      }
+      Assert.That(actualDraws, Is.Not.Zero);
+    }
+
+    private static string ToDiscard(int discard)
+    {
+      var i = discard / 9;
+      var c = discard % 9;
+      return (char)('1' + c) + "mpsz".Substring(i, 1);
+    }
+
+    private static string ToHand(IReadOnlyList<int> concealed)
+    {
+      return ToHand(concealed.Slice(0, 9), 'm') +
+             ToHand(concealed.Slice(9, 9), 'p') +
+             ToHand(concealed.Slice(18, 9), 's') +
+             ToHand(concealed.Slice(27, 7), 'z');
+    }
+
+    private static string ToHand(IReadOnlyList<int> slice, char suit)
+    {
+      var sb = new StringBuilder();
+      for (var i = 0; i < slice.Count; ++i)
+      {
+        for (var j = 0; j < slice[i]; ++j)
+        {
+          sb.Append((char)('1' + i));
+        }
+      }
+      sb.Append(suit);
+      return sb.ToString();
     }
 
     private static IEnumerable<Tuple<int[], int[]>> GetChinitsuHands()
