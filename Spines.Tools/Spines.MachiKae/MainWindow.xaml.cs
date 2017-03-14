392cd418859d2e7317eb3a924df994851a8c8907
spinesheath <spines@web.de>
2017-03-14 21:44:56 +0100

    Created MachiKae project

---
+++ Spines.Tools/Spines.MachiKae/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+
+namespace Spines.MachiKae
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
