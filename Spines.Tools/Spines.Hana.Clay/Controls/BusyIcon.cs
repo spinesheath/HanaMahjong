1465f85e0dcdba211bd3dc4b95b88d1b6221a760
spinesheath <spines@web.de>
2017-03-18 08:36:38 +0100

    BusyIcon

---
+++ Spines.Tools/Spines.Hana.Clay/Controls/BusyIcon.cs
@@ -0,0 +1,16 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Spines.Hana.Clay.Controls
+{
+  public class BusyIcon : Control
+  {
+    static BusyIcon()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyIcon), new FrameworkPropertyMetadata(typeof(BusyIcon)));
+    }
+  }
+}
