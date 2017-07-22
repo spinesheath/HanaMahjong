194fffaaf3f1f3b9a710969675119336ae7cff55
spinesheath <spines@web.de>
2017-07-22 13:43:37 +0200

    ASP.Net Core individual user template

---
+++ Spines.Hana.Blame/Spines.Hana.Blame/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spines.Hana.Blame.Models.ManageViewModels
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
