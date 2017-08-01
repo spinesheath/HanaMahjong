86a6ae789894975d12ad1943c2284aade780639e
spinesheath <spines@web.de>
2017-08-01 21:25:39 +0200

    Display randomly generated comments

--- Spines.Hana.Blame/Spines.Hana.Blame/Controllers/HomeController.cs
+++ Spines.Hana.Blame/Spines.Hana.Blame/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿// This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Spines.Hana.Blame.Models.Wwyd;
+using Spines.Hana.Blame.Models.ThreadViewModels;
 
 namespace Spines.Hana.Blame.Controllers
 {
@@ -10,6 +12,10 @@ namespace Spines.Hana.Blame.Controllers
   {
     public IActionResult Index()
     {
+      var r = new Random(DateTime.Now.Millisecond);
+      var count = r.Next(5, 10);
+      var values = Enumerable.Range(0, count).Select(x => r.Next());
+      ViewData["Thread"] = new ThreadViewModel(values.Select(c => c.ToString()));
       return View();
     }
 
@@ -22,17 +28,6 @@ namespace Spines.Hana.Blame.Controllers
 
     public IActionResult Error()
     {
-      return View();
-    }
-
-    public IActionResult Wwyd(string h)
-    {
-      var hand = WwydHand.Parse(h);
-      if (hand.IsValid)
-      {
-        ViewData["Hand"] = hand.NormalizedRepresentation;
-      }
-
       return View();
     }
   }
---
+++ Spines.Hana.Blame/Spines.Hana.Blame/Models/ThreadViewModels/ThreadViewModel.cs
@@ -0,0 +1,18 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spines.Hana.Blame.Models.ThreadViewModels
+{
+  public class ThreadViewModel
+  {
+    public ThreadViewModel(IEnumerable<string> comments)
+    {
+      Comments = comments.ToList();
+    }
+
+    public List<string> Comments { get; }
+  }
+}
