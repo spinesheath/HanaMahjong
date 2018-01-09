4d36f4932b77b20f4fe0179b620b3ab7ca151e2b
spinesheath <spines@web.de>
2018-01-09 17:45:56 +0100

    sort blame search results #49

--- Spines.Hana.Blame/Spines.Hana.Blame/wwwroot/js/browse.js
+++ Spines.Hana.Blame/Spines.Hana.Blame/wwwroot/js/browse.js
@@ -9,6 +9,11 @@
 }
 
 function updateBrowseResults(data) {
-    const results = data.map(r => `<div class='browse-result-div'><button class='browse-result' data-replayid='${r.id}' onclick='navigateToReplay(this.dataset.replayid)'><div class='browse-result-header'>${r.participants.join("   ")}</div><div class=browse-result-body>${new Date(r.timestamp).toLocaleString()}<div></button></div>`).join("\n");
+    const sorted = data.sort(replaySort);
+    const results = sorted.map(r => `<div class='browse-result-div'><button class='browse-result' data-replayid='${r.id}' onclick='navigateToReplay(this.dataset.replayid)'><div class='browse-result-header'>${r.participants.join("   ")}</div><div class=browse-result-body>${new Date(r.timestamp).toLocaleString()}<div></button></div>`).join("\n");
     $("#browseResultsDiv").html(results);
+}
+
+function replaySort(a, b) {
+    return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
 }
